1ed14308d6b8f8a8ce75a1f6917e19550a69319e
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2024-06-04 11:14:51 +0200

    email validation

---
+++ section 13/13_email_validation/scripts.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const emailValidation = /\w+@\w+\.\w+/;
+
+console.log(emailValidation.test("deyvison@gmail.com"));
+console.log(emailValidation.test("ghessi@gmail.com"));
+console.log(emailValidation.test("gmail.com"));
