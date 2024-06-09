559f823e318e92d2fa8928e6016b31769efe788d
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2024-06-09 21:49:58 +0200

    callback function

---
+++ section 14/01_callback/scripts.js
@@ -0,0 +1,30 @@
+console.log("1");
+
+setTimeout(function() {
+console.log("1")
+}, 1000);
+
+setTimeout(function() {
+console.log("2")
+}, 2000);
+
+setTimeout(function() {
+console.log("3")
+}, 3000);
+
+setTimeout(function() {
+console.log("4")
+}, 4000);
+
+setTimeout(function() {
+console.log("5")
+}, 5000);
+
+setTimeout(function() {
+console.log("6")
+}, 6000);
+
+
+console.log("2");
+console.log("3");
+console.log("4");
